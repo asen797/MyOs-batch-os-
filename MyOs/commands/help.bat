e82147782016eb2ed23264e2231c5368e15b4081
Asen <158561774+asen797@users.noreply.github.com>
2024-07-04 19:50:30 +0300

    help.bat

--- MyOs/commands/help.bat
+++ MyOs/commands/help.bat
@@ -5,7 +5,6 @@ echo nameChange - change the name that you put as pc name
 echo apps - (still in develop) quick accses to the apps that you have 
 echo parrot - dancing parrot
 echo helpp - open te commands that i made(the rest are cmd commands;)
-echo animation -(still in develop) make your own animation,when the animation is done close the window because it will not stop when you want ;)
 echo -------------------------------------------------------------------------------------------------------------------------------------------
 echo type exit to go back
 
